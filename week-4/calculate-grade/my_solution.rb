6e1c7bde4f6ee164a11bb8d9409703d752577350
drumguy16 <ripley.brett@gmail.com>
2016-01-21 01:25:58 -0500

    Add 4.2 and 4.3 challenges

---
+++ week-4/calculate-grade/my_solution.rb
@@ -0,0 +1,22 @@
+# Calculate a Grade
+
+# I worked on this challenge [by myself, with: ].
+def get_grade(grade)
+	if grade >=90
+		p "A"
+	elsif grade >=80
+		p "B"
+	elsif grade >=70
+		p "C" 
+	elsif grade >=60
+		p "D"	
+	elsif grade >=50
+		p "F"	
+	else
+		p "F"
+	end
+end
+
+
+
+# Your Solution Below
