c3231fd80f7225107e0d94710c633ab5a7fa4537
drumguy16 <ripley.brett@gmail.com>
2016-01-25 08:00:36 -0500

    Add 4.7 pairing challenge

---
+++ week-4/add-it-up/my_solution.rb
@@ -0,0 +1,53 @@
+# Add it up!
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented in the file.
+
+# I worked on this challenge [by myself, with: ].
+
+# 0. total Pseudocode
+# make sure all pseudocode is commented out!
+
+# Input:
+# Output:
+# Steps to solve the problem.
+
+
+# 1. total initial solution
+
+	
+
+def total(array)
+  sum = 0.0
+  result = 0.0
+  if array.length > 0 then
+  array.each do |item|
+  sum += item
+  end
+  result = sum 
+  end
+  return result
+end
+
+
+# 3. total refactored solution
+
+
+
+# 4. sentence_maker pseudocode
+# make sure all pseudocode is commented out!
+# Input:
+# Output:
+# Steps to solve the problem.
+
+
+# 5. sentence_maker initial solution
+
+	
+
+def sentence_maker(array)
+  string = array.join(' ').capitalize! + '.'
+end
+
+# 6. sentence_maker refactored solution
---
+++ week-4/factorial/my_solution.rb
@@ -0,0 +1,14 @@
+
+# Factorial
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+def factorial(num)
+  if num <= 1
+  1
+  else
+  num * factorial(num-1)
+  end
+end
